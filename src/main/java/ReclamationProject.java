5763d8f490306eaa6faaeaa3a7250a18866ca406
17729 <17729@BB>
2017-09-26 14:38:53 -0500

    lab4

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -8,19 +8,21 @@
  * 5. Remove unnecessary comments as appropriate
  */
 
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
-        /*
-         * For loop with i
-         */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+public class ReclamationProject {
+    static String doit(String firstString,String secondString){
+        if (firstString.length() > secondString.length()){
+            String c = firstString; firstString=secondString; secondString=c;
+            }
+        String r = "";
+        for (int i = 0; i < firstString.length(); i++) {
+            for (int j = firstString.length() - i; j > 0; j--) {
+                for (int k = 0; k < secondString.length()- j; k++) {
+                    if (firstString.regionMatches(i, secondString, k, j) && j >r.length()) {
+                        r = firstString.substring(i,i + j);
+                    }
+                }
+            }
+        }
+        return r;
+        }
 }
